bb875502dc6758f38717efae33aa84ebf14dd540
Konstantin Azizov <azizovkostya97@gmail.com>
2016-07-13 13:38:43 +0300

    Add domain disabling/enabling functionality

--- js/listener.js
+++ js/listener.js
@@ -86,4 +86,18 @@ function checkEvent(event) {
   }
 }
 
-document.addEventListener('keyup', checkEvent);
+chrome.storage.sync.get('disabled_sites', function(data) {
+  if(data.disabled_sites.indexOf(window.location.hostname) === -1) {
+    document.addEventListener('keyup', checkEvent);
+  }
+});
+
+// Updating subscribtion status for `keyup` event
+// if domain disabled or enabled for extension
+chrome.storage.onChanged.addListener(function(changes) {
+  if(changes.disabled_sites.newValue.indexOf(window.location.hostname) > -1) {
+    document.removeEventListener('keyup', checkEvent);
+  } else {
+    document.addEventListener('keyup', checkEvent);
+  }
+});
---
+++ js/popup.js
@@ -0,0 +1,36 @@
+function initialize() {
+  var checkbox = document.getElementById('enabled-checkbox');
+  var hostname = '';
+  var sites = [];
+
+  // Getting url of current tab
+  chrome.tabs.getSelected(null, function(tab) {
+    hostname = /:\/\/([^\/]+)/i.exec(tab.url)[1];
+
+    // Check if current url exists in `disabled_sites` list
+    chrome.storage.sync.get('disabled_sites', function(data) {
+      sites = data.disabled_sites || sites;
+
+      if(sites.indexOf(hostname) === -1) {
+        checkbox.checked = true;
+      }
+    });
+  });
+
+
+  checkbox.addEventListener('click', function() {
+    if(checkbox.checked === false) {
+      sites.push(hostname);
+    } else if(sites.indexOf(hostname) > -1) {
+      sites.splice(sites.indexOf(hostname), 1);
+    }
+
+    chrome.storage.sync.set({
+      disabled_sites: sites
+    }, function() {
+      console.log('Saved');
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
