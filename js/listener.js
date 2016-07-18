c8e33a5965ad0dd3c3fe4fa7edb8c072cb2314db
Konstantin Azizov <azizovkostya97@gmail.com>
2016-07-18 11:23:58 +0300

    Update expression detection
    Now it's just detects if last symbol before space was number and so, it's less complitated now

--- js/listener.js
+++ js/listener.js
@@ -1,4 +1,3 @@
-var buffer = '';
 var lastItem;
 var mathRegexp = /([0-9]|\*|\/|\+|-|\.|\(|\)){3,}/g;
 
@@ -48,12 +47,18 @@ function findClosest(string, regexp, position) {
   return lastMatch;
 }
 
-function isNumberOrOperator(char) {
-  return '0123456789-+*/.()'.indexOf(char) > -1;
-}
+/**
+ * @param  {HTML element}  element
+ * @return {Boolean}
+ */
+function isReadyToReplace(element) {
+  if(element && element.value) {
+    var lastSymbol = element.value[element.selectionStart - 2];
 
-function isReadyToReplace(string) {
-  return string.length > 2; // TODO: Just dummy check for something like 2+2 should be replaced with something more complicated
+    return /[0-9]/.test(lastSymbol);
+  } else {
+    return false;
+  }
 }
 
 function checkEvent(event) {
@@ -63,39 +68,33 @@ function checkEvent(event) {
     return;
   }
 
-  if(isNumberOrOperator(event.key)) {
-    buffer += event.key;
-  } else {
-    if(event.code === 'Space' && isReadyToReplace(buffer)) {
-      var value = target.value + event.key;
-      var closestItem = findClosest(value, mathRegexp, target.selectionStart);
+  if(event.code === 'Space' && isReadyToReplace(target)) {
+    var value = target.value + event.key;
+    var closestItem = findClosest(value, mathRegexp, target.selectionStart);
 
-      if(closestItem) {
-        try {
-          var result = eval(closestItem[0]);
+    if(closestItem) {
+      try {
+        var result = eval(closestItem[0]);
 
-          lastItem = closestItem;
-          lastItem.target = target;
-          lastItem.result = result;
+        lastItem = closestItem;
+        lastItem.target = target;
+        lastItem.result = result;
 
-          target.value = value.replaceRange(closestItem.index,
-            closestItem.index + closestItem[0].length + 1,
-            result);
-        } catch(err) { }
-      }
-    } else if(event.code === 'Escape' && lastItem.length) { // Last action was canceled
-      lastItem.target.value = lastItem.target.value.replaceRange(lastItem.index,
-        lastItem.index + lastItem.result.toString().length + 1,
-        lastItem[0]);
-
-      // Set focus back to the field because Esc reset focus to body page
-      lastItem.target.focus();
-      lastItem = null;
-    } else {
-      lastItem = null;
+        target.value = value.replaceRange(closestItem.index,
+          closestItem.index + closestItem[0].length + 1,
+          result);
+      } catch(err) { }
     }
+  } else if(event.code === 'Escape' && lastItem.length) { // Last action was canceled
+    lastItem.target.value = lastItem.target.value.replaceRange(lastItem.index,
+      lastItem.index + lastItem.result.toString().length + 1,
+      lastItem[0]);
 
-    buffer = '';
+    // Set focus back to the field because Esc reset focus to body page
+    lastItem.target.focus();
+    lastItem = null;
+  } else {
+    lastItem = null;
   }
 }
 
